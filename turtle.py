37dabe78a406ac45b69b5233afab1a62aa4f95b6
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-05-28 13:41:12 +0600

    learned while loop

---
+++ turtle.py
@@ -0,0 +1,10 @@
+import turtle 
+
+wn = turtle.Screen()       # Capital 'S' in Screen
+alex = turtle.Turtle()     # Capital 'T' in Turtle
+
+alex.forward(150)
+alex.left(90)
+alex.forward(75)
+
+wn.exitonclick()           # Use the same variable name 'wn'
